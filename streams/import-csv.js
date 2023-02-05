ce762108f2ea0eb09c54d6b4591ed2d8f0d1b9ed
Raphael Berger <raphaelmb@gmail.com>
2023-02-05 14:04:00 -0300

    Add csv read stream

---
+++ streams/import-csv.js
@@ -0,0 +1,32 @@
+import { parse } from "csv-parse";
+import fs from "node:fs";
+
+const csvPath = new URL("./tasks.csv", import.meta.url);
+
+const stream = fs.createReadStream(csvPath);
+
+const csvParse = parse({
+  delimiter: ",",
+  skipEmptyLines: true,
+  fromLine: 2,
+});
+
+(async () => {
+  const url = "http://localhost:3333/tasks";
+  const linesParse = stream.pipe(csvParse);
+
+  for await (const line of linesParse) {
+    const [title, description] = line;
+
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+    });
+  }
+})();
